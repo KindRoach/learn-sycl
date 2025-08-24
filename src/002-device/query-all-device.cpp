bf883e5eeb9434674a2fbb01ec60097f065e8462
KindRoach <kindroach@hotmail.com>
2025-08-24 17:12:15 +0800

    Add SLM Size to query-all-device.cpp

--- src/002-device/query-all-device.cpp
+++ src/002-device/query-all-device.cpp
@@ -10,7 +10,8 @@ int main() {
                 << "\tVendor: " << device.get_info<sycl::info::device::vendor>() << std::endl
                 << "\tBackend: " << backend_to_string(device.get_backend()) << std::endl
                 << "\tType: " << device_type_to_string(device.get_info<sycl::info::device::device_type>()) << std::endl
-                << "\tCompute Unit: " << device.get_info<sycl::info::device::max_compute_units>() << std::endl;
+                << "\tCompute Unit: " << device.get_info<sycl::info::device::max_compute_units>() << std::endl
+                << "\tSLM Size(bytes): " << device.get_info<sycl::info::device::local_mem_size>() << std::endl;
         print_sub_groups(device);
     }
 }
