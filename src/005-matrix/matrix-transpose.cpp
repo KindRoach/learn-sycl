e672c77d1088ecbbae25987dbbb161207e773f43
KindRoach <kindroach@hotmail.com>
2025-09-01 22:22:21 +0800

    Add matrix_transpose_nd_range_tile_vec to matrix-transpose.cpp

--- src/005-matrix/matrix-transpose.cpp
+++ src/005-matrix/matrix-transpose.cpp
@@ -108,6 +108,39 @@ void matrix_transpose_nd_range_write_continue_vec(sycl::queue &q, T *in, T *out,
         });
 }
 
+template<
+    typename T,
+    uint16_t WG_SIZE,
+    uint8_t SG_SIZE,
+    uint8_t WI_SIZE
+>
+void matrix_transpose_nd_range_tile_vec(sycl::queue &q, T *in, T *out, size_t m, size_t n) {
+    q.parallel_for(
+        sycl::nd_range<2>{{m / WI_SIZE, n / WI_SIZE}, {WG_SIZE, WG_SIZE}},
+        [=](sycl::nd_item<2> item) [[sycl::reqd_sub_group_size(SG_SIZE)]] {
+            size_t i = item.get_global_id(0);
+            size_t j = item.get_global_id(1);
+
+            T *base_in = in + i * WI_SIZE * n + j * WI_SIZE;
+            T *base_out = out + j * WI_SIZE * m + i * WI_SIZE;
+
+            sycl::vec<T, WI_SIZE> vec[WI_SIZE];
+            for (size_t k = 0; k < WI_SIZE; ++k) {
+                vec[k].load(0, base_in + k * n);
+            }
+
+            for (size_t k_i = 0; k_i < WI_SIZE; ++k_i) {
+                for (size_t k_j = k_i + 1; k_j < WI_SIZE; ++k_j) {
+                    std::swap(vec[k_i][k_j], vec[k_j][k_i]);
+                }
+            }
+
+            for (size_t k = 0; k < WI_SIZE; ++k) {
+                vec[k].store(0, base_out + k * m);
+            }
+        });
+}
+
 
 int main() {
     using dtype = float;
@@ -155,6 +188,10 @@ int main() {
         {
             "matrix_transpose_nd_range_write_continue_vec",
             matrix_transpose_nd_range_write_continue_vec<dtype, wg_size, sg_size, wi_size>
+        },
+        {
+            "matrix_transpose_nd_range_tile_vec",
+            matrix_transpose_nd_range_tile_vec<dtype, wg_size, sg_size, wi_size>
         },
     };
 
