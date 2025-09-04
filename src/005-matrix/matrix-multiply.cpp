37d9d606611263cd594cafb02e75a81a8dd3d6f8
KindRoach <kindroach@hotmail.com>
2025-09-04 22:19:52 +0800

    Add matrix_multiply_nd_range_vec to matrix-multiply.cpp

--- src/005-matrix/matrix-multiply.cpp
+++ src/005-matrix/matrix-multiply.cpp
@@ -17,8 +17,8 @@ void matrix_multiply_ref(
     for (size_t i = 0; i < m; i++) {
         for (size_t j = 0; j < n; j++) {
             T sum = 0;
-            for (size_t k_i = 0; k_i < k; k_i++) {
-                sum += a[i * k + k_i] * b[j * k + k_i];
+            for (size_t p = 0; p < k; p++) {
+                sum += a[i * k + p] * b[j * k + p];
             }
             c[i * n + j] = sum;
         }
@@ -31,8 +31,8 @@ void matrix_multiply_naive(sycl::queue &q, T *a, T *b, T *c, size_t m, size_t n,
         T sum = 0;
         size_t i = idx[0];
         size_t j = idx[1];
-        for (size_t k_i = 0; k_i < n; k_i++) {
-            sum += a[i * k + k_i] * b[j * k + k_i];
+        for (size_t p = 0; p < k; p++) {
+            sum += a[i * k + p] * b[j * k + p];
         }
         c[i * n + j] = sum;
     });
@@ -49,16 +49,43 @@ void matrix_multiply_nd_range(sycl::queue &q, T *a, T *b, T *c, size_t m, size_t
             T sum = 0;
             size_t i = item.get_global_id(0);
             size_t j = item.get_global_id(1);
-            for (size_t k_i = 0; k_i < n; k_i++) {
-                sum += a[i * k + k_i] * b[j * k + k_i];
+            for (size_t p = 0; p < k; p++) {
+                sum += a[i * k + p] * b[j * k + p];
             }
             c[i * n + j] = sum;
         });
 }
 
+template<
+    typename T,
+    uint16_t WG_SIZE,
+    uint8_t WI_SIZE
+>
+void matrix_multiply_nd_range_vec(sycl::queue &q, T *a, T *b, T *c, size_t m, size_t n, size_t k) {
+    q.parallel_for(
+        sycl::nd_range<2>{{m, n}, {WG_SIZE, WG_SIZE}},
+        [=](sycl::nd_item<2> item) {
+            size_t i = item.get_global_id(0);
+            size_t j = item.get_global_id(1);
+            sycl::vec<T, WI_SIZE> vec_a, vec_b, vec_c{0};
+            for (size_t p = 0; p < k; p += WI_SIZE) {
+                vec_a.load(0, a + i * k + p);
+                vec_b.load(0, b + j * k + p);
+                vec_c += vec_a * vec_b;
+            }
+
+            T sum = 0;
+            for (int p = 0; p < WI_SIZE; ++p) {
+                sum += vec_c[p];
+            }
+            c[i * n + j] = sum;
+        });
+}
+
+
 int main() {
     using dtype = float;
-    size_t loop = 10;
+    size_t loop = 1000;
     size_t m = 1024, n = 1024, k = 1024; // 2G Flops
 
     std::vector<dtype> a(m * k), b(k * n), c(m * n);
@@ -86,6 +113,10 @@ int main() {
         {
             "matrix_multiply_nd_range",
             matrix_multiply_nd_range<dtype, 32>
+        },
+        {
+            "matrix_multiply_nd_range_vec",
+            matrix_multiply_nd_range_vec<dtype, 32, 4>
         }
     };
 
