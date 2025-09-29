351b8b9cf73739558dbb61fd82597b11f06bfe14
KindRoach <kindroach@hotmail.com>
2025-09-29 17:07:49 +0800

    Add matrix_vector_multiply_n_split_sg/wg.

--- src/005-matrix/matrix-vector-multiply.cpp
+++ src/005-matrix/matrix-vector-multiply.cpp
@@ -11,11 +11,11 @@ void matrix_vector_multiply_ref(std::vector<T> &a, std::vector<T> &b, std::vecto
     size_t ld = a_layout == row_major ? n : m;
     for (size_t i = 0; i < m; i++) {
         T sum = 0;
-        for (size_t j = 0; j < n; j++) {
+        for (size_t k = 0; k < n; k++) {
             if constexpr (a_layout == row_major) {
-                sum += mat(a.data(), ld, i, j) * b[j];
+                sum += mat(a.data(), ld, i, k) * b[k];
             } else {
-                sum += mat(a.data(), ld, j, i) * b[j];
+                sum += mat(a.data(), ld, k, i) * b[k];
             }
         }
         c[i] = sum;
@@ -29,11 +29,11 @@ void matrix_vector_multiply_naive(sycl::queue &q, T *a, T *b, T *c, size_t m, si
         sycl::range<1>(m),
         [=](sycl::id<1> i) {
             T sum = 0;
-            for (size_t j = 0; j < n; j++) {
+            for (size_t k = 0; k < n; k++) {
                 if constexpr (a_layout == row_major) {
-                    sum += mat(a, ld, i, j) * b[j];
+                    sum += mat(a, ld, i, k) * b[k];
                 } else {
-                    sum += mat(a, ld, j, i) * b[j];
+                    sum += mat(a, ld, k, i) * b[k];
                 }
             }
             c[i] = sum;
@@ -50,14 +50,79 @@ void matrix_vector_multiply_nd_range(sycl::queue &q, T *a, T *b, T *c, size_t m,
         [=](sycl::nd_item<1> item) [[sycl::reqd_sub_group_size(SG_SIZE)]] {
             T sum = 0;
             size_t i = item.get_global_id();
-            for (size_t j = 0; j < n; j++) {
+            for (size_t k = 0; k < n; k++) {
                 if constexpr (a_layout == row_major) {
-                    sum += mat(a, ld, i, j) * b[j];
+                    sum += mat(a, ld, i, k) * b[k];
                 } else {
-                    sum += mat(a, ld, j, i) * b[j];
+                    sum += mat(a, ld, k, i) * b[k];
                 }
             }
             c[i] = sum;
+        });
+}
+
+template<typename T, layout a_layout, size_t WG_SIZE, size_t SG_SIZE>
+void matrix_vector_multiply_n_split_sg(sycl::queue &q, T *a, T *b, T *c, size_t m, size_t n) {
+    check_divisible(m, WG_SIZE, "M must be divisible by WG_SIZE");
+    check_divisible(n, SG_SIZE, "N must be divisible by SG_SIZE");
+
+    size_t ld = a_layout == row_major ? n : m;
+    q.parallel_for(
+        sycl::nd_range<2>{{m, SG_SIZE}, {WG_SIZE, SG_SIZE}},
+        [=](sycl::nd_item<2> item) [[sycl::reqd_sub_group_size(SG_SIZE)]] {
+            size_t i = item.get_global_id(0);
+
+            auto sg = item.get_sub_group();
+            size_t sg_i = sg.get_local_linear_id();
+
+            T sum = 0;
+            for (size_t k = 0; k < n; k += SG_SIZE) {
+                if constexpr (a_layout == row_major) {
+                    sum += mat(a, ld, i, k + sg_i) * b[k + sg_i];
+                } else {
+                    sum += mat(a, ld, k + sg_i, i) * b[k + sg_i];
+                }
+            }
+
+            T sg_sum = sycl::reduce_over_group(sg, sum, sycl::plus<>());
+
+            if (sg_i == 0) {
+                c[i] = sg_sum;
+            }
+        });
+}
+
+template<typename T, layout a_layout, size_t WG_SIZE, size_t SG_SIZE>
+void matrix_vector_multiply_n_split_wg(sycl::queue &q, T *a, T *b, T *c, size_t m, size_t n) {
+    check_divisible(n, WG_SIZE, "N must be divisible by WG_SIZE");
+
+    size_t ld = a_layout == row_major ? n : m;
+    size_t ele_per_sg = n / (WG_SIZE / SG_SIZE);
+    q.parallel_for(
+        sycl::nd_range<2>{{m, WG_SIZE}, {1, WG_SIZE}},
+        [=](sycl::nd_item<2> item) [[sycl::reqd_sub_group_size(SG_SIZE)]] {
+            size_t i = item.get_global_id(0);
+
+            auto sg = item.get_sub_group();
+            size_t sg_i = sg.get_local_linear_id();
+            size_t sg_group_id = sg.get_group_id();
+            size_t start_id = sg_group_id * ele_per_sg;
+            size_t end_id = start_id + ele_per_sg;
+
+            T sum = 0;
+            for (size_t k = start_id; k < end_id; k += SG_SIZE) {
+                if constexpr (a_layout == row_major) {
+                    sum += mat(a, ld, i, k + sg_i) * b[k + sg_i];
+                } else {
+                    sum += mat(a, ld, k + sg_i, i) * b[k + sg_i];
+                }
+            }
+
+            T sg_sum = sycl::reduce_over_group(item.get_group(), sum, sycl::plus<>());
+
+            if (item.get_group().leader()) {
+                c[i] = sg_sum;
+            }
         });
 }
 
@@ -68,9 +133,7 @@ void test_matrix_multiply() {
     std::cout << "-------------- matrix a in " << a_major << " --------------\n";
 
     using dtype = float;
-    constexpr uint16_t wg_size = 32;
     constexpr uint8_t sg_size = 32;
-    constexpr uint8_t wi_size = 4;
 
     size_t secs = 10;
     size_t m = 512 * 1024, n = 1024; // 1G FLOPs
@@ -94,7 +157,9 @@ void test_matrix_multiply() {
     using func_t = std::function<void(sycl::queue &, dtype *, dtype *, dtype *, size_t, size_t)>;
     std::vector<std::tuple<std::string, func_t> > funcs{
         {"matrix_vector_multiply_naive", matrix_vector_multiply_naive<dtype, a_layout>},
-        {"matrix_vector_multiply_nd_range", matrix_vector_multiply_nd_range<dtype, a_layout, wg_size, sg_size>},
+        {"matrix_vector_multiply_nd_range", matrix_vector_multiply_nd_range<dtype, a_layout, 256, sg_size>},
+        {"matrix_vector_multiply_n_split_sg", matrix_vector_multiply_n_split_sg<dtype, a_layout, 32, sg_size>},
+        {"matrix_vector_multiply_n_split_wg", matrix_vector_multiply_n_split_wg<dtype, a_layout, 256, sg_size>},
     };
 
     for (auto [func_name,func]: funcs) {
